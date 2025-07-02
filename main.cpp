104f0e64c2c71e9b16b42f66a3526be6f9ad30f9
pedrofalconi-ux <pedrofalconi11@gmail.com>
2025-07-02 18:55:53 -0300

    feat: Foi adicionada a função 'Boa tarde'

--- main.cpp
+++ main.cpp
@@ -4,9 +4,15 @@
 using namespace std;
 
 int main() {
+    char seunome[30]; 
     int x = 10;
     string nome = "Joao";
     string linguagem = "C++";
 
     cout << nome << " X: " << x << " Linguagem usada: " << linguagem << endl;
+    cout << "Qual é o seu nome?\n";
+    scanf("%s", &seunome); 
+    printf("Boa tarde %s", seunome);
+
+    return 0; 
 }
